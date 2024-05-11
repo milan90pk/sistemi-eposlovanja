84bd787b8c409becef213aaf112651101b48f2ff
milan90pk <milan.ognjanovic.20@singimail.rs>
2024-05-11 15:51:21 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello world");
+import cors from 'cors'
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+const port = 4000
+app.listen(port,()=>{
+    console.log("App started and listening on "+port)
+})
+
+app.get("/",(req,res)=>{
+    res.json({
+        message: "Hello World From ExpressJS and Typescript"
+    })
+})
+
+
+app.get("*",(req,res)=>{
+    res.status(404).json({
+        message: "Not found"
+    })
+})
+
